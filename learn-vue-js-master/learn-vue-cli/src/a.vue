30f722fa4c75a2054d771921b9e22268f9e44154
yonghoon-thevc <yonghoon@thevc.kr>
2022-10-20 10:05:19 +0900

    Add: 싱글 파일 컴포넌트 적용법

---
+++ learn-vue-js-master/learn-vue-cli/src/a.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- html -->
+  <div></div>
+</template>
+<script>
+// javascript
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
+
+<style>
+/* css */
+</style>
