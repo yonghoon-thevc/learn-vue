8853f18248b3585096c467cfeb6f10704d37d4d5
yonghoon-thevc <yonghoon@thevc.kr>
2022-10-20 10:28:58 +0900

    Add: 싱글 파일 컴포넌트

--- learn-vue-js-master/learn-vue-cli/src/App.vue
+++ learn-vue-js-master/learn-vue-cli/src/App.vue
@@ -1,34 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <!-- 태그 작성 방식 -->
-    <!-- <hello-world></hello-world>
-    <HelloWorld></HelloWorld>
-    <HelloWorld /> -->
-  </div>
+  <div>{{ str }}</div>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  // instance 옵션 속성 or component 옵션 속성
-  name: "App",
-  components: {
-    // 'hello-world': HelloWorld
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      str: "hi",
+    };
   },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
