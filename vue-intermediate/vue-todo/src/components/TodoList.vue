f6c6909da4ed1327607bf1276eeb2e41b233916b
yonghoon-thevc <yonghoon@thevc.kr>
2022-10-20 14:17:34 +0900

    Add: Todo List Localstorage 데이터 출력

--- vue-intermediate/vue-todo/src/components/TodoList.vue
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -1,18 +1,29 @@
 <template>
-  <div>List</div>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
-  components: {},
   data() {
     return {
-      sampleData: "",
+      todoItems: [],
     };
   },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
+  // Vue 인스턴스가 생성될 때 호출, React useEffect와 비슷
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i += 1) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
   methods: {},
 };
 </script>
