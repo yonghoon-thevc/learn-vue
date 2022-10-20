3a9db31432ae243212388569b821e79c09f3b713
yonghoon-thevc <yonghoon@thevc.kr>
2022-10-20 10:42:31 +0900

    Add: event emit

--- learn-vue-js-master/learn-vue-cli/src/App.vue
+++ learn-vue-js-master/learn-vue-cli/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     {{ str }}
     <!-- <app-header v-bind:프롭스속성이름=""></app-header> -->
-    <app-header v-bind:propsdata="str"></app-header>
+    <app-header v-bind:propsdata="str" @renew="renewStr"></app-header>
   </div>
 </template>
 <script>
@@ -17,10 +17,10 @@ export default {
       str: "Header",
     };
   },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
-  methods: {},
+  methods: {
+    renewStr: function () {
+      this.str = "hello";
+    },
+  },
 };
 </script>
--- learn-vue-js-master/learn-vue-cli/src/components/AppHeader.vue
+++ learn-vue-js-master/learn-vue-cli/src/components/AppHeader.vue
@@ -1,11 +1,17 @@
 <template>
   <header>
     <h1>{{ propsdata }}</h1>
+    <button @click="sendEvent">send</button>
   </header>
 </template>
 
 <script>
 export default {
   props: ["propsdata"],
+  methods: {
+    sendEvent: function () {
+      this.$emit("renew");
+    },
+  },
 };
 </script>
