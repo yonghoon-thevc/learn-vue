5b95734152af22ff1c77ca873b6bb1c35b75cd25
yonghoon-thevc <yonghoon@thevc.kr>
2022-10-20 10:47:45 +0900

    Add: 프로젝트 생성 및 마크업

---
+++ learn-vue-js-master/learn-vue-form/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="">
+    <div>
+      <label for="userName">id: </label>
+      <input id="userName" type="text" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      sampleData: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+};
+</script>
