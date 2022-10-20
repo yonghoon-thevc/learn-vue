43122868382088712610f8a6ac8f6a4aad74c430
yonghoon-thevc <yonghoon@thevc.kr>
2022-10-20 14:08:41 +0900

    Add: input Css 적용

--- vue-intermediate/vue-todo/src/components/TodoInput.vue
+++ vue-intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem" />
+  <div class="inputBox" shadow>
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" />
     <button @click="addTodo">add</button>
+    <!-- <span class="addContainer">
+      <i class="fas fa-plus addBtn"></i>
+    </span> -->
   </div>
 </template>
 <script>
@@ -16,8 +19,40 @@ export default {
       console.log(this.newTodoItem);
       // 저장하는 로직
       localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
       this.newTodoItem = "";
     },
   },
 };
 </script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  widows: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
