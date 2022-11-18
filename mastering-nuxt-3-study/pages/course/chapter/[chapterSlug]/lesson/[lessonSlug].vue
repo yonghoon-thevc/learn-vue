3f4614eeb1c728142864a44f7ffe9ba3e94b84d8
yonghoon-thevc <yonghoon@thevc.kr>
2022-11-18 13:16:34 +0900

    Add: Loading in Course Data

--- mastering-nuxt-3-study/pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ mastering-nuxt-3-study/pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,6 +1,45 @@
 <template>
-  <h1>제목</h1>
-  <p>내용</p>
-  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+        v-if="lesson.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        href="lesson.sourceUrl"
+      >
+        Download Source Code
+      </a>
+      <a
+        v-if="lesson.downloadUrl"
+        class="font-normal text-md text-gray-500"
+        href="lesson.downloadUrl"
+      >
+        Download Video
+      </a>
+    </div>
+    <p>{{ lesson.text }}</p>
+  </div>
 </template>
+
+<script setup>
+import { useCourse } from "~~/composables/useCourse";
+
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+
+const lesson = computed(() => {
+  return chapter.value.lessons.find(
+    (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
+console.log(course);
+</script>
