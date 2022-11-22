fb989833ae1c18636e2459ff479b0de43de341d7
yonghoon-thevc <yonghoon@thevc.kr>
2022-11-22 18:03:49 +0900

    Add: 헤더와 검색 UI 스타일 정리

--- nuxt-start/components/SearchInput.vue
+++ nuxt-start/components/SearchInput.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
+  <div class="input-wrapper flex">
     <input
       type="text"
-      :value="searchKeyword"
+      class="search-input"
+      :value="value"
       @input="$emit('input', $event.target.value)"
     />
-    <button type="button" @click="$emit('search')">Search</button>
+    <button class="btn" type="button" @click="$emit('search')">search</button>
   </div>
 </template>
+
 <script>
 export default {
   props: {
@@ -18,3 +20,19 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.input-wrapper {
+  height: 40px;
+  margin: 1rem 0;
+}
+.search-input {
+  width: 200px;
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+.btn {
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+</style>
--- nuxt-start/layouts/default.vue
+++ nuxt-start/layouts/default.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <header>
-      <h1>
-        <NuxtLink to="/"> Nuxt Shopping </NuxtLink>
-      </h1>
+    <header class="title">
+      <NuxtLink class="logo" to="/"> Nuxt Shopping </NuxtLink>
     </header>
     <Nuxt />
   </div>
 </template>
+
 <script>
 export default {};
 </script>
+
+<style scoped>
+header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+}
+.logo {
+  font-weight: 900;
+}
+.logo:visited {
+  color: inherit;
+}
+</style>
