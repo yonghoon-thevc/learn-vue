22f5fc2960be05b6480bd87f9917c2acf3696f95
yonghoon-thevc <yonghoon@thevc.kr>
2022-11-22 17:56:39 +0900

    Add: 검색 기능 마무리

--- nuxt-start/pages/index.vue
+++ nuxt-start/pages/index.vue
@@ -53,9 +53,10 @@ export default {
     },
     async searchProducts() {
       const response = await fetchProductsByKeyword(this.searchKeyword);
-      const products = response.data;
-      console.log(products);
-      return { products };
+      this.products = response.data.map((item) => ({
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      }));
     },
   },
 };
