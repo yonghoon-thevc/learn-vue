27ec6b996f39c7e86ff849c71e4159b51218e165
yonghoon-thevc <yonghoon@thevc.kr>
2023-02-23 14:26:21 +0900

    Feat: dynamic component

--- nuxt3-get-started/app.vue
+++ nuxt3-get-started/app.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <HomeWelcome />
-    <Counter />
+    <a @click="view('home')">Home</a>
+    <a @click="view('counter')">Counter</a>
+    
+    <HomeWelcome v-if="page === 'home'" />
+    <Counter v-if="page === 'counter'" />
+    
+    <!-- <button @click="toggle">Toggle</button> -->
+    <!-- <div v-if="MyComponent === 'welcome'">
+      <HomeWelcome />
+    </div>
+    <Counter v-else /> -->
+    <!-- <component :is="MyComponent" /> -->
   </div>
 </template>
+
+<script setup>
+// const MyComponent = ref(resolveComponent('Counter'));
+// const MyComponent = ref('counter');
+
+// const toggle = () => {
+//   MyComponent.value = 'welcome';
+// }
+const page = ref('home')
+const view = (name) => {
+  page.value = name
+}
+</script>
