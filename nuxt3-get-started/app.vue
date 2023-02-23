ab3107ef8ece4609a13201e721ab11b269b4497e
yonghoon-thevc <yonghoon@thevc.kr>
2023-02-23 14:10:12 +0900

    Add: new nuxt3 project

---
+++ nuxt3-get-started/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <HomeWelcome />
+    <Counter />
+  </div>
+</template>
---
+++ nuxt3-get-started/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="increment">increment</button>
+    <p>{{ count }}</p>
+    <button @click="decrement">decrement</button>
+  </div>
+</template>
+
+<script setup>
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+}
+const decrement = () => {
+  count.value--;
+}
+</script>
